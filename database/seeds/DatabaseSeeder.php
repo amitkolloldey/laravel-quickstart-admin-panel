ec492890bfc30b4a7481aceb8e6a48615bae036a
amitkolloldey <amitkolloldey@gmail.com>
2018-04-15 02:54:02 +0600

    All crud working including media

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'id' => '1',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'status' => '1',
+            'role_id' => '1',
+            'photo_id' => '1',
+            'password' => bcrypt('123456'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+        DB::table('posts')->insert([
+            'id' => '1',
+            'title' => 'Hello World',
+            'body' => 'Hello World From Laravel',
+            'category_id' => '1',
+            'user_id' => '1',
+            'photo_id' => '1',
+            'status' => '1',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        DB::table('roles')->insert([
+            'id' => '1',
+            'name' => 'administrator',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        DB::table('photos')->insert([
+            'id' => '1',
+            'path' => 'placeholder.png',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        DB::table('categories')->insert([
+            'id' => '1',
+            'name' => 'UnCategorized',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
     }
 }
